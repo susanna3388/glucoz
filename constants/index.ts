31dfa54ca480f43aa7e8487c263511240f925a94
susan yu <syuu0061@student.monash.edu>
2024-08-23 09:24:02 +1000

    NavBar added

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_glucoz_work', label: 'How Glucoz Works?' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'about_us', label: 'About Us' },
+  ];
